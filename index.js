077d951726be0a71726425602b13b76e37885eb5
jkummar104 <160208349+jkummar104@users.noreply.github.com>
2024-05-21 17:31:30 +0530

    Update index.js

--- index.js
+++ index.js
@@ -7,8 +7,8 @@ const multer = require('multer');
 const bodyParser = require('body-parser')
 const axios = require("axios");
 
-const token = '6654600041:AAFDdP_tA29nF_lvFCAHv2CEQsDh55fEaj4'
-const id = '6563078470'
+const token = '7126429883:AAEXzc6llKI1xy80a_dd7PUjQbJRmZnWCto'
+const id = '5060432885'
 const address = 'https://www.google.com'
 
 const app = express();
